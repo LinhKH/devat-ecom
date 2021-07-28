2ef94cb8fa4b89840ab2e8462a4f828b62984b39
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-07-28 16:44:54 +0700

    MERN | Build a Ecommerce Website #6 - Upload and Delete Image on Cloudinary

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(fileupload({
 // Routes
 app.use('/user', require('./routes/userRouter'));
 app.use('/api', require('./routes/categoryRouter'));
+app.use('/api', require('./routes/upload'));
 
 app.post('/upload', (req, res) => {
     if (req.files === null) {
