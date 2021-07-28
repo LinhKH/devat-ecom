d92308cf7d0cb1f911d7a63b75d010e178122891
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-07-28 11:35:41 +0700

    MERN | Build a Ecommerce Website #5 - Category(2)

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
+import axios from 'axios';
+
+const FileUpload = () => {
+    const [file, setFile] = useState('');
+    const [fileName, setFilename] = useState('Choose File');
+    const [uploadFile, setUploadFile] = useState({});
+    const [message, setMessage] = useState('');
+    const [uploadPercentage, setUploadPercentage] = useState(0);
+
+    const onChange = (e) => {
+        setFile(e.target.files[0]);
+        setFilename(e.target.files[0].name);
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const res = await axios.post('/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                onUploadProgress: progressEvent => {
+                    setUploadPercentage(
+                        parseInt(
+                            Math.round((progressEvent.loaded * 100) / progressEvent.total)
+                        )
+                    );
+                }
+            });
+            // Clear percentage
+            setTimeout(() => setUploadPercentage(0), 10000);
+
+            const { fileName, filePath } = res.data;
+            setUploadFile({ fileName, filePath });
+            setFilename('');
+            setMessage('File Uploaded.');
+        } catch (err) {
+            if (err.response.status === 500) {
+                setMessage("There was a problem with the server.");
+            } else {
+                setMessage(err.response.data.msg);
+            }
+            setUploadPercentage(0)
+        }
+
+    }
+
+    return (
+        <>
+            {message ? <Message msg={message} /> : null}
+            <form onSubmit={onSubmit}>
+                <div className="custom-file">
+                    <input type="file" className="custom-file-upload" id="customFile" onChange={onChange} />
+                    <label className="custom-file-label" htmlFor="customFile">
+                        {fileName}
+                    </label>
+                </div>
+                {uploadPercentage !== 0 ? <Progress percentage={uploadPercentage} /> : null}
+                <input type="submit" value="Upload" className="btn btn-primary btn-clock mt-4" />
+            </form>
+            {uploadFile ? <div className="row mt-5">
+                <div className="col-md-6 m-auto">
+                    <h3 className="text-center">{uploadFile.fileName}</h3>
+                    <img style={{ width: '100%' }} src={uploadFile.filePath} />
+                </div>
+            </div> : null}
+        </>
+    )
+}
+
+export default FileUpload
---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(fileupload({
+    useTempFiles: true
+}));
+
+// Routes
+app.use('/user', require('./routes/userRouter'));
+app.use('/api', require('./routes/categoryRouter'));
+
+app.post('/upload', (req, res) => {
+    if (req.files === null) {
+        return res.status(400).json({msg: "No File Upload."});
+    }
+
+    const file = req.files.file;
+    const file_name = new Date().getTime()+file.name;
+    file.mv(`${__dirname}/client/public/uploads/${file_name}`, err => {
+        if (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
+        res.json({fileName: file_name, filePath: `/uploads/${file_name}`});
+    });
+});
+
+// Connect to mongodb
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
