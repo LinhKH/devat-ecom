f42fb8483187e37d6ccf2aa8b50889520fec2329
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-07-29 11:34:09 +0700

    MERN | Build a Ecommerce Website #8 - Products(2) - Filter - Sort - Pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,10 +1,57 @@
 const Product = require('../models/productModel');
 
+// Filter, sorting and paginating
+class APIfeatures {
+    constructor(query, queryString) {
+        this.query = query;
+        this.queryString = queryString;
+    }
+    filtering() {
+        const queryObj = { ...this.queryString }
+        const excludedFields = ['page', 'sort', 'limit'];
+        excludedFields.forEach(el => delete(queryObj[el]));
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match => '$' + match);
+        console.log({queryStr});
+
+        this.query.find(JSON.parse(queryStr));
+
+        return this;
+    }
+    sorting() {
+        if (this.queryString.sort) {
+            const sortBy = this.queryString.sort.split(',').join(' ');
+            console.log(sortBy)
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-createdAt');
+        }
+
+        return this;
+    }
+    paginating() {
+        const page = this.queryString.page * 1 || 1;
+        const limit = this.queryString.limit * 1 || 3;
+        const skip = (page - 1) * limit;
+        this.query = this.query.skip(skip).limit(limit);
+        return this;
+    }
+}
 const productCtrl = {
     getProducts: async (req, res) => {
         try {
-            const products = await Product.find();
-            return res.json(products);
+            const objApi = new APIfeatures(Product.find(), req.query);
+            const productsTotal = await objApi.query;
+            const total = productsTotal.length;
+            const features = objApi.filtering().sorting().paginating();
+            const products = await features.query;
+            return res.json({
+                status: 'success',
+                total,
+                result: products.length,
+                products: products
+            });
         } catch (error) {
             return res.status(400).json({ msg: error.message });
         }
